a00e619425d96aef2aa3f83140cd721a1e30691d
argi47 <argi47@msn.com>
2024-07-13 17:06:05 +0200

    creacion nuevo hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useReducer, useEffect, useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import Form from './components/Form'
 import CalorieTracker from './components/CalorieTracker'
 import ActivityList from './components/ActivityList'
-import { activityReducer, initialState } from './reducers/activity-reducer'
+import { useActivity } from './hooks/useActivity'
 
 function App() {
 
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  const { state, dispatch } = useActivity()
 
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(state.activities))
